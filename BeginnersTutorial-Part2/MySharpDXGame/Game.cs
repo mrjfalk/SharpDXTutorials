0a53fd57db07529000f4bfc04293ade72622d066
Johan Falk <johanfalk@hotmail.com>
2015-08-29 14:05:49 +0200

    Added source for beginners tutorial part 2.

---
+++ BeginnersTutorial-Part2/MySharpDXGame/Game.cs
@@ -0,0 +1,40 @@
+﻿using SharpDX.Windows;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharpDXGame
+{
+	public class Game : IDisposable
+	{
+		private RenderForm renderForm;
+
+		private const int Width = 1280;
+		private const int Height = 720;
+
+		public Game()
+		{
+			renderForm = new RenderForm("My first SharpDX game");
+			renderForm.ClientSize = new Size(Width, Height);
+			renderForm.AllowUserResizing = false;
+		}
+
+		public void Run()
+		{
+			RenderLoop.Run(renderForm, RenderCallback);
+		}
+
+		private void RenderCallback()
+		{
+
+		}
+
+		public void Dispose()
+		{
+			renderForm.Dispose();
+		}
+	}
+}
